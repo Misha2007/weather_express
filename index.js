f33281510e09b414b28f2c2a42e019ece4251e4f
Misha2007 <mishadrogovoz@gmail.com>
2024-09-23 11:36:54 +0300

    Created API error handling

--- index.js
+++ index.js
@@ -31,7 +31,8 @@ const getWeatherDataPromise = (url) => {
             let result = {
                 description: description,
                 city: city,
-                temp: temp
+                temp: temp,
+                error: null
             } 
             resolve(result)
         })
@@ -53,6 +54,9 @@ app.all('/', function(req, res) {
     getWeatherDataPromise(url)
     .then(data => {
         res.render('index', data)
+    }) 
+    .catch(error => {
+        res.render('index', {error: 'Problem with getting data, try again'} )
     }) 
 })
 
