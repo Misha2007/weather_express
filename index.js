d35d03e21616fdee1d076c49b38dcec56b1be5d3
Misha2007 <mishadrogovoz@gmail.com>
2024-09-23 10:40:13 +0300

    Created form request responce with API data

--- index.js
+++ index.js
@@ -10,7 +10,31 @@ app.set('views', path.join(__dirname, 'views'))
 const key = '2127b877044b42365c14225f6b322cde'
 let city = 'Tartu'
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
 app.get('/', function(req, res) {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
+    .then(response => { 
+        return response.json() 
+    })
+    .then(data => {
+        let description = data.weather[0].description
+        let city = data.name
+        let temp = Math.round(parseFloat(data.main.temp) - 273.15)
+        console.log(description);
+        console.log(temp);
+        console.log(city);
+        res.render('index', {
+            description: description,
+            city: city,
+            temp: temp
+        })
+    }) 
+})
+
+app.post('/', function(req, res) {
+    let city = req.body.cityname
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`)
     .then(response => { 
         return response.json() 
